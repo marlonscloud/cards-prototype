7be9935114200aa3585e47cd500be4680b916c39
Marlon Dedakis <marlon.dedakis@method4.co.uk>
2021-06-23 12:56:31 +0100

    styling working on clicks

--- cards-prototype.js
+++ cards-prototype.js
@@ -10,23 +10,8 @@ function CardListeners() {
 }
 
 function CardOpen() {
-    /*When any card is opened, all other cards should close (only one open at a time)*/
-    var openCards = document.querySelectorAll(".large-card");
-    var openCardsParagraph = document.querySelectorAll(".occupations-text__padding-right");
-
-    for (i = 0; i < openCards.length; i++) {
-        openCards[i].classList.remove("large-card");
-    }
-
-    for (i = 0; i < openCardsParagraph.length; i++) {
-        openCardsParagraph[i].classList.remove("occupations-text__padding-right");
-    }
-
-    /*Hide the last empty column if it's not already hidden*/
     var occupationsColumn = this.closest(".occupations-columns").querySelector(".omaps-occupations--empty-column");
-    if (!occupationsColumn.classList.contains("omaps-occupations--empty-column__hidden")) {
-        occupationsColumn.classList.toggle("omaps-occupations--empty-column__hidden");
-    }
+    HandleClicks(occupationsColumn);
 
     /*Add listeners back into any other cards that were closed above*/
     let cards = document.getElementsByClassName("small-card");
@@ -35,7 +20,11 @@ function CardOpen() {
     /*Set omaps-occupations column (flex column) to expand*/
     this.parentElement.parentElement.classList.toggle("omaps-occupations__wide");
     /*Set padding on the occupations-text paragraph so that it doesn't take up the new larger width of the column*/
-    this.previousSibling.previousSibling.firstElementChild.classList.toggle("occupations-text__padding-right");
+    var paragraphsToResize = this.closest(".occupations-columns").querySelectorAll(".occupations-text-paragraph");
+    for (i = 0; i < paragraphsToResize.length; i++) {
+        paragraphsToResize[i].classList.add("occupations-text__padding-right");
+    }
+    // this.previousSibling.previousSibling.firstElementChild.classList.toggle("occupations-text__padding-right");
     this.classList.toggle("large-card");
 
     /*Remove listeners to open card (since it's now open)*/
@@ -52,14 +41,18 @@ function CardClose() {
     /*Get current card*/
     var smallCard = this.closest(".small-card");
 
+    /*Set padding on the occupations-text paragraph so that it doesn't take up the new larger width of the column*/
+    var openCardsParagraph = document.querySelectorAll(".occupations-text__padding-right");
+    for (i = 0; i < openCardsParagraph.length; i++) {
+        openCardsParagraph[i].classList.remove("occupations-text__padding-right");
+    }
+
     /*show the last empty column*/
     smallCard.closest(".occupations-columns").querySelector(".omaps-occupations--empty-column").classList.toggle("omaps-occupations--empty-column__hidden");
 
     /*Set styles to shrink column*/
     smallCard.classList.toggle("large-card");
     smallCard.parentElement.parentElement.classList.toggle("omaps-occupations__narrow");
-    /*Set padding on the occupations-text paragraph so that it doesn't take up the new larger width of the column*/
-    smallCard.previousSibling.previousSibling.firstElementChild.classList.toggle("occupations-text__padding-right");
 
     /*Remove any leftover open listener on card and set-up a new one so we can bind 'this'
     - we also need to store the new bound instance of this listener function so we can later remove it on open*/
@@ -81,6 +74,26 @@ function CardClose() {
         smallCard.addEventListener("click", temp = CardOpen.bind(smallCard));
         window.cardListeners.push(temp);
     }.bind(smallCard), 100);
+}
+
+function HandleClicks(occupationsColumn) {
+    /*When any card is opened, all other cards should close (only one open at a time)*/
+    var openCards = document.querySelectorAll(".large-card");
+    var openCardsParagraph = document.querySelectorAll(".occupations-text__padding-right");
+
+    for (i = 0; i < openCards.length; i++) {
+        openCards[i].classList.remove("large-card");
+    }
+
+    for (i = 0; i < openCardsParagraph.length; i++) {
+        openCardsParagraph[i].classList.remove("occupations-text__padding-right");
+    }
+
+    /*Hide the last empty column if it's not already hidden*/
+
+    if (!occupationsColumn.classList.contains("omaps-occupations--empty-column__hidden")) {
+        occupationsColumn.classList.toggle("omaps-occupations--empty-column__hidden");
+    }
 }
 
 /*Utilities START*/
