2edd0533af3e63f67f3f05c265959c41f1127045
Marlon Dedakis <marlon.dedakis@method4.co.uk>
2021-06-23 09:34:27 +0100

    commented code for future use

--- cards-prototype.js
+++ cards-prototype.js
@@ -3,22 +3,34 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function CardListeners() {
+    /*When the page loads, get all cards and add the card open event listener to each one*/
     let cards = document.getElementsByClassName("small-card");
     addEventListenerList(cards, 'click', CardOpen);
 }
 
 function CardOpen() {
+    /*Set styles to expand column*/
     this.parentElement.parentElement.style.width = "55%";
     this.classList.toggle("large-card");
+
+    /*Remove listeners to open card (since it's now open)*/
     this.removeEventListener("click", CardOpen);
     this.removeEventListener("click", window.boundCardOpenFn);
+
+    /*Add event listener to svg (close icon) for closing card*/
     this.firstElementChild.addEventListener("click", CardClose);
 }
 
 function CardClose() {
+    /*Get current card*/
     var smallCard = this.closest(".small-card");
+
+    /*Set styles to shrink column*/
     smallCard.classList.toggle("large-card");
     smallCard.parentElement.parentElement.style.width = "33%";
+
+    /*Remove any leftover open listener on card and set-up a new one so we can bind 'this'
+    - we also need to store the new bound instance of this listener function so we can later remove it on open*/
     smallCard.removeEventListener("click", CardOpen);
     setTimeout(function () {
         smallCard.addEventListener("click", window.boundCardOpenFn = CardOpen.bind(smallCard));
